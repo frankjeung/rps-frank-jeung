31083d86d32d91a042321103fd076c71bcd5db1a
frankjeung <97686278+frankjeung@users.noreply.github.com>
2022-02-01 19:39:07 -0500

    Asked for user input

--- game.py
+++ game.py
@@ -5,3 +5,26 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+# ASK FOR USER INPUT
+
+
+user_choice = input("Please choose one of: 'rock', 'paper', 'scissors' : ")
+
+print("USER CHOSE:", user_choice)
+
+
+# VALIDATIONS
+
+
+
+# COMPUTER CHOICE
+
+
+
+# DETERMINE THE WINNER
+
+
+
+# FINAL RESULTS
